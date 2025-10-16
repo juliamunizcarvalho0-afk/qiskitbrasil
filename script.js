72d08f5ed9197dea01007c22e8f36f28b7b6f6d1
qiskitbrasil <quiskitbrasil@gmail.com>
2025-10-16 18:47:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ dayCards.forEach(card => {
       const item = icon.closest('.timeline-item');
       if (!item) return;
 
-      const isMobile = window.innerWidth <= 768; // breakpoint mobile
       const isAlreadyOpen = item.classList.contains('open');
 
       // Fecha todos os itens e painel atual
@@ -43,7 +42,8 @@ dayCards.forEach(card => {
       panel.innerHTML = '';
       container.classList.remove('expanded-line');
 
-      if (isAlreadyOpen || isMobile) return; // no mobile, não expande nem mantém aberto
+      // Se o mesmo ícone foi clicado, só fecha e sai
+      if (isAlreadyOpen) return;
 
       // Caso contrário, abre o painel
       const content = item.querySelector('.timeline-content-item');
@@ -82,7 +82,6 @@ dayCards.forEach(card => {
     }
   });
 })();
-
 
 
 // ====== MENU MOBILE ======
